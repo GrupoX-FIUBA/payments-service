5a5489e3527d0234fbfb8ac18401ce3e945c5832
ezezbogar <ezezbogar@gmail.com>
2022-07-07 16:08:54 -0300

    db disable ssl

--- src/db/db.js
+++ src/db/db.js
@@ -3,7 +3,7 @@ const { Sequelize } = require("sequelize");
 const sequelize = new Sequelize(process.env.DATABASE_URL, {
   dialectOptions: {
     ssl: {
-      require: true,
+      require: false,
       rejectUnauthorized: false,
     },
   },
